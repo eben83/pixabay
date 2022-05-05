8a0220dbbdeedaca26a7ec24740a170bd05f8171
Eben Burger <eben@arlowconsulting.co.za>
2022-05-05 22:03:37 +0200

    app scss added

--- pixabay/src/App.js
+++ pixabay/src/App.js
@@ -1,7 +1,8 @@
-import './App.xs.css'
 import {Route} from "react-router-dom";
 import Layout from "./components/layout/layout";
 import Home from "./components/home/home";
+
+import './App.xs.css'
 
 const App = () => {
 
