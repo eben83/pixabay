01f8717566ca1622c109e88c82655a707016c81a
Eben Burger <eben@arlowconsulting.co.za>
2022-05-05 22:03:19 +0200

    image UI and scss added to all components

---
+++ pixabay/src/components/image/image.js
@@ -0,0 +1,16 @@
+﻿import React, {} from 'react';
+
+import './image.xs.css'
+
+const Image = ({src, alt}) => {
+  return (
+    <>
+      <div className='image'>
+        <div className='previewImage'>
+          <img src={src} alt={alt} />
+        </div>
+      </div>
+    </>
+  );
+}
+export default Image;
