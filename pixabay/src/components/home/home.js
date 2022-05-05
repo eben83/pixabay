4d854cc1440768e03c3994d8b2111837052c083c
Eben Burger <eben@arlowconsulting.co.za>
2022-05-05 22:04:46 +0200

    home UI updated

--- pixabay/src/components/home/home.js
+++ pixabay/src/components/home/home.js
@@ -1,11 +1,32 @@
-import React, {} from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import Image from "../image/image";
 
-const Home = (props) => {
+import './home.xs.css'
+import {Container} from "react-bootstrap";
+
+const Home = () => {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios.get('https://pixabay.com/api/?key=6473511-0417f2cad683f1bee54cafe15')
+      .then((response) => {
+        setData(response.data.hits)
+        console.log("DATA", response.data)
+      })
+  }, [])
+  console.log("DATA 2", data)
+  
   return (
     <>
-      <div className='d-flex justify-content-center w-100'>
-        <h4 >Home</h4>
-      </div>
+      <Container fluid className='home'>
+        <div className='imageContainer'>
+          {data && data.map((item) => {
+            return (
+              <Image src={item.previewURL} alt={'test'} />
+            )
+          })}
+        </div>
+      </Container>
     </>
   );
 }
