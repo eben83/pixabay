0f33c59abda103601ce779038fee78e6b62a1409
Eben Burger <eben@arlowconsulting.co.za>
2022-05-07 18:30:12 +0200

    updated image styling

--- pixabay/src/components/image.js
+++ pixabay/src/components/image.js
@@ -15,5 +15,37 @@ const Image = ({src, alt}) => {
 
 const Wrapper = styled.div`
   padding: 0.5rem;
+  
+  img {
+    width: 8rem;
+    height: 8rem;
+    object-fit: cover;
+    box-shadow:
+            0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+            0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+            0 12.5px 10px rgba(0, 0, 0, 0.06),
+            0 22.3px 17.9px rgba(0, 0, 0, 0.072),
+            0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+            0 100px 80px rgba(0, 0, 0, 0.12);
+  }
+  @media(min-width: 576px) {
+    img {
+      width: 15rem;
+      height: 15rem;
+    }
+  }
+  @media(min-width: 768px) {
+    img {
+      width: 20rem;
+      height: 20rem;
+      margin-bottom: 2.5rem;
+    }
+  }
+  @media(min-width: 992px) {
+    img {
+      width: 30rem;
+      height: 30rem;
+    }
+  }
 `
 export default Image;
