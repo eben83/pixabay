2e89fadd89171b151c82cec81ed2bb33437154fe
Eben Burger <eben@arlowconsulting.co.za>
2022-05-07 18:35:59 +0200

    layout styling added

--- pixabay/src/components/layout.js
+++ pixabay/src/components/layout.js
@@ -1,13 +1,18 @@
 import React, {} from 'react';
+import styled from "styled-components";
 
 const Layout = ({children}) => {
   return (
     <>
-      <div className='layout'>
+      <Wrapper>
         {children}
-      </div>
+      </Wrapper>
     </>
   );
 }
+
+const Wrapper = styled.div`
+  background: #F8F8F8;
+`
 
 export default Layout;
